7a9708f0f9b7b60c0d7e74a696a79363698d00ee
DANG QUANG HUY <huydang.dh@outlook.com.vn>
2023-05-17 10:27:21 +0000

    v1.5.1

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store, {} from './store.ts';
+import store, { } from './store.ts';
 import LoginPage from './pages/Login.tsx';
 import 'w3-css'
 import WorkTimeRecord from './pages/app/worktime_record.tsx';
@@ -13,6 +13,7 @@ import 'react-datetime-picker/dist/DateTimePicker.css';
 import 'react-calendar/dist/Calendar.css';
 import 'react-clock/dist/Clock.css';
 
+export const MyContext = createContext({})
 
 const router = createBrowserRouter([
   {
@@ -32,19 +33,20 @@ const router = createBrowserRouter([
   }
 ]);
 
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <div className="w3-bar w3-black">
-        <a href="/" className="w3-bar-item w3-button">Home</a>
-        <a href="#" className="w3-bar-item w3-button">Link 1</a>
-        <a href="#" className="w3-bar-item w3-button">Link 2</a>
-        <a href="#" className="w3-bar-item w3-button">Link 3</a>
-      </div>
-      <div className='w3-container'>
-        <RouterProvider router={router} />
-      </div>
+      <MyContext.Provider value={{}}>
+        <div className="w3-bar w3-black">
+          <a href="/" className="w3-bar-item w3-button">Home</a>
+          <a href="#" className="w3-bar-item w3-button">Link 1</a>
+          <a href="#" className="w3-bar-item w3-button">Link 2</a>
+          <a href="#" className="w3-bar-item w3-button">Link 3</a>
+        </div>
+        <div className='w3-container'>
+          <RouterProvider router={router} />
+        </div>
+      </MyContext.Provider>
     </Provider>
   </React.StrictMode>,
 )
