1869d4b3aab01c8a269716314e51ddfe005526ca
huydangdh <huydang.dh@outlook.com.vn>
2023-05-26 12:50:31 +0000

    v1.5.91

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 import { Provider, useDispatch } from "react-redux";
 import store, { AppDispatch, MesUser, resetUser, setUser, useMesSelector } from "./store.ts";
 import LoginPage from "./pages/Login.tsx";
@@ -65,16 +65,33 @@ const router = createBrowserRouter([
 
 function DoLogout() {
   store.dispatch(resetUser({}))
+  window.location.replace("/")
+
+}
+
+function w3_open() {
+  document.getElementById("mySidebar")!.style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar")!.style.display = "none";
 }
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <div className="w3-bar w3-black">
+        <button className="w3-bar-item w3-green" onClick={w3_open}>&#9776;</button>
         <a href="/" className="w3-bar-item w3-button">Home</a>
-        <a href="#" className="w3-bar-item w3-button">Link 1</a>
         <a href="#" className="w3-bar-item w3-button">Link 2</a>
         <a href="#" className="w3-bar-item w3-button w3-red" onClick={DoLogout}>Logout</a>
+      </div>
+      <div className="w3-sidebar w3-bar-block w3-collapse w3-card" id="mySidebar">
+        <button className="w3-bar-item w3-button w3-hide-large"
+          onClick={w3_close}>Close &times;</button>
+        <a href="#" className="w3-bar-item w3-button">Link 1</a>
+        <a href="#" className="w3-bar-item w3-button">Link 2</a>
+        <a href="#" className="w3-bar-item w3-button">Link 3</a>
       </div>
       <div className="w3-container">
         <MyRouter></MyRouter>
