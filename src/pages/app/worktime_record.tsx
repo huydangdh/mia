448bcccacecc311d6087cd135b816929db5d0dd6
huydang <huydang.dh@gmail.com>
2023-05-27 19:45:19 +0800

    v1.5.93

--- src/pages/app/worktime_record.tsx
+++ src/pages/app/worktime_record.tsx
@@ -1,42 +1,59 @@
 import { useState } from "react";
-import DateTimePicker from "react-datetime-picker";
 import { useMesSelector } from "../../store";
 import { Navigate } from "react-router-dom";
+import DateTimePicker from "react-datetime-picker";
+import { Button, Card, Container, Form, Stack } from "react-bootstrap";
+
 
 function WorkTimeRecord() {
-  const mesUser = useMesSelector((state)=> state.mesUserState.user)
+  const mesUser = useMesSelector((state) => state.mesUserState.user)
   const [dtStartTime, onChangeStartTime] = useState(new Date());
   const [dtEndTime, onChangeEndTime] = useState(new Date());
 
   function doSend(_event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
-    alert(_event)   
+    alert(_event)
   }
-  if(!mesUser.isAuthed) return <Navigate to="/Login" /> 
+
+  function WorkTimeRecordForm() {
+    return (
+      <Card>
+        <Card.Header as="h5">WORKTIME_RECORD</Card.Header>
+        <Card.Body>
+          <Card.Title></Card.Title>
+          <Form className="">
+            <Stack>
+              <Form.Group>
+                <Form.Label> Start_Time: </Form.Label>
+                <DateTimePicker onChange={onChangeStartTime} value={dtStartTime}></DateTimePicker>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label> End_Time: </Form.Label>
+                <DateTimePicker onChange={onChangeEndTime} value={dtEndTime}></DateTimePicker>
+              </Form.Group>
+              <Button variant="primary" onClick={doSend}>Send</Button>
+            </Stack>
+          </Form>
+        </Card.Body>
+      </Card>
+    );
+  }
+
+
+  if (!mesUser.isAuthed) return <Navigate to="/Login" />
   return (
     <>
       <div>
-        <div className="w3-card-4">
-          <div className="w3-container w3-green">
-            <h2>Input WorkTimeRecord</h2>
-          </div>
-          <form className="w3-container">
-            <p>
-              <label>Start_Time: </label>
-              <DateTimePicker onChange={onChangeStartTime} value={dtStartTime}></DateTimePicker>
-            </p>
-            <p>
-              <label>End_Time: </label>
-              <DateTimePicker onChange={onChangeEndTime} value={dtEndTime} ></DateTimePicker>
-            </p>
-            <button type="button" onClick={doSend}>Send</button>
-            <p></p>
-          </form>
+        <div className="">
+          <Container>
+            <WorkTimeRecordForm></WorkTimeRecordForm>
+          </Container>
+
 
         </div>
       </div>
     </>
 
-)
+  )
 }
 
 export default WorkTimeRecord
